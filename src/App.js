3afdcaeaf24d5e9718c055185de405be561c0e50
cleomark <cleocalitisin@gmail.com>
2024-05-31 08:16:06 +0800

    add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      {" "}
+      {/* React Fragment */}
+      <header>
+        {" "}
+        {/* Semantic tag for header section */}
+        {/* Your header content here */}
+      </header>
+      <nav>
+        {" "}
+        {/* Semantic tag for navigation section */}
+        {/* Your navigation content here */}
+      </nav>
+      <main>
+        {" "}
+        {/* Semantic tag for main content section */}
+        <div className="App">
+          {" "}
+          {/* Regular div for specific content within main */}
+          Homepage
+        </div>
+        {/* Other content sections within main */}
+      </main>
+      <footer>
+        {" "}
+        {/* Semantic tag for footer section */}
+        {/* Your footer content here */}
+      </footer>
+    </>
+  );
 }
 
 export default App;
