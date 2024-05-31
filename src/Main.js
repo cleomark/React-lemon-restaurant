7bd79e2e7adc7073dc020d695c4c5cb619402f89
cleomark <cleocalitisin@gmail.com>
2024-05-31 08:17:16 +0800

    add main component

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <div className="App">Homepage</div>
+      {/* Other content sections within main */}
+    </main>
+  );
+}
+
+export default Main;
